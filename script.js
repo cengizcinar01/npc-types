057e65eb412069437edaed884585f0328441b4c6
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-28 17:55:04 +0100

    Added Animation and Improved Drawing in JavaScript Code
    The changes involve adding animation to the game object with frameX, maxFrame, frameInterval, and frameTimer variables. The object's frame updates based on the frameInterval. In the draw function, the image now considers the current frame. Also, the starting point of drawing has been shifted to the center of the object's width.

--- script.js
+++ script.js
@@ -44,13 +44,24 @@ document.addEventListener('DOMContentLoaded', function () {
         constructor(game) {
             this.game = game;
             this.markedForDeletion = false;
+            this.frameX;
+            this.maxFrame = 5;
+            this.frameInterval = 100;
+            this.frameTimer = 0;
         }
         update(deltaTime) {
             this.x -= this.vx * deltaTime;
             if (this.x < 0 - this.width) this.markedForDeletion = true;
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX < this.maxFrame) this.frameX++;
+                else this.frameX = 0;
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
         }
         draw(ctx) {
-            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
     }
 
@@ -116,8 +127,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         draw(ctx) {
             ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(this.x, this.y);
+            ctx.moveTo(this.x + this.width / 2, 0);
+            ctx.lineTo(this.x + this.width / 2, this.y + 10);
             ctx.stroke();
             super.draw(ctx);
         }
